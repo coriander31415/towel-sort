4ed3a4db3fadbe2b39846da51070e0562e809345
coriander31415 <coriander31415@gmail.com>
2020-10-20 19:17:25 +0300

    feat: solve towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    return matrix
+        .map((val, row) =>
+            (row % 2 === 0) ? val : val.reverse())
+        .flat();
 }
